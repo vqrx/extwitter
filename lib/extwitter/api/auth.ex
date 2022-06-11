9db06c2a7ac1c5ae88feba59221fc7f226d6e82a
x <x@x.com>
2022-06-12 02:45:57 +0300

    Send oauth_token in access_token query params
    instead of body

--- lib/extwitter/api/auth.ex
+++ lib/extwitter/api/auth.ex
@@ -31,8 +31,21 @@ defmodule ExTwitter.API.Auth do
 
   def access_token(verifier, request_token) do
     oauth = ExTwitter.Config.get_tuples |> verify_params
-    response = ExTwitter.OAuth.request(:post, request_url("oauth/access_token"),
-      [oauth_verifier: verifier], oauth[:consumer_key], oauth[:consumer_secret], request_token, nil)
+
+    credentials = OAuther.credentials(
+        consumer_key: oauth[:consumer_key],
+        consumer_secret: oauth[:consumer_secret]
+    )
+    base_url = request_url("oauth/access_token")
+    params = [{"oauth_token", request_token}, {"oauth_verifier", verifier}]
+    signed_params = OAuther.sign("post", base_url, params, credentials)
+
+    {header, req_params} = OAuther.header(signed_params)
+    header = header |> Tuple.to_list() |> Enum.map(&to_charlist/1) |> List.to_tuple()
+    url = base_url <> "?" <> URI.encode_query(req_params)
+    request = {to_charlist(url), [header], 'application/x-www-form-urlencoded', ""}
+
+    response = ExTwitter.OAuth.send_httpc_request(:post, request, [])
     case response do
       {:ok, {{_, 200, _}, _headers, body}} ->
         access_token = Elixir.URI.decode_query(to_string body)
@@ -40,8 +53,8 @@ defmodule ExTwitter.API.Auth do
         |> Enum.into(%{})
         |> ExTwitter.Parser.parse_access_token
         {:ok, access_token}
-      {:ok, {{_, code, _}, _, _}} ->
-        {:error, code}
+      {:ok, res}->
+        {:error, res}
     end
   end
 end
